83a79ebb298229a957f2988714b782c022c8838a
David Munyiri <davidmumyiri2019@gmail.com>
2022-11-14 03:24:47 -0800

    first commit

---
+++ intro.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+int number,cube;
+printf("Enter a number to calculate it's cube\n");
+scanf("%d",&length);
+cube=(number*number*number);
+if((number%2)!=0 ){
+    printf("cannot calculate the cube\n");
+}
+else{
+    printf("%d=%d*%d*%d","Cube\n");
+}
+}
